167866ac94b1e56db834bce577d99d3af90ad5bb
Benjamin Obi Tayo <benjaminobi@gmail.com>
2018-08-30 07:13:50 -0500

    Add files via upload

---
+++ prob_distributions.R
@@ -0,0 +1,22 @@
+#code to plot binomial, normal and poisson probability distributions
+
+#created on 8/30/2018
+
+#@ Benjamin Tayo
+
+
+library(tidyverse)
+N<-100
+n<-1:N
+p<-0.5
+mean<-N*p
+sd<-sqrt(N*p*(1-p))
+
+pdat<-data.frame(n=c(n,n,n),prob=c(dbinom(n,N,p),dnorm(n,mean,sd),dpois(n,mean)),
+                 type=sort(rep(c("binomial","normal","poison"),times=N)))
+
+#plot distributions on same graph
+pdat%>%mutate(type=factor(type))%>%ggplot(aes(n,prob,color=type))+geom_point(size=2)+geom_line(size=0.75)
+
+#plot distributions side by side
+pdat%>%mutate(type=factor(type))%>%ggplot(aes(n,prob))+geom_point(size=2)+geom_line(size=0.75)+facet_wrap(~type)
